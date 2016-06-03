65353f039d732eccea1079742f25dc08f9b67c31
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2016-06-03 14:17:17 +0200

    Added send and recv definitions.

--- include/reverseFunctions.hpp
+++ include/reverseFunctions.hpp
@@ -5,6 +5,20 @@
 namespace medi {
 
   template<typename DATATYPE>
+  void TAMPI_Send_adj(typename DATATYPE::AdjointType* bufAdjoints, typename DATATYPE::PassiveType* bufPrimals, int bufSize, int count, int dest, int tag, TAMPI_Comm comm) {
+    MEDI_UNUSED(bufPrimals);
+    MEDI_UNUSED(count);
+    MPI_Recv(bufAdjoints, bufSize, MPI_BYTE, dest, tag, comm, MPI_STATUS_IGNORE);
+  }
+
+  template<typename DATATYPE>
+  void TAMPI_Recv_adj(typename DATATYPE::AdjointType* bufAdjoints, typename DATATYPE::PassiveType* bufPrimals, int bufSize, int count, int dest, int tag, TAMPI_Comm comm) {
+    MEDI_UNUSED(bufPrimals);
+    MEDI_UNUSED(count);
+    MPI_Send(bufAdjoints, bufSize, MPI_BYTE, dest, tag, comm);
+  }
+
+  template<typename DATATYPE>
   void TAMPI_Reduce_adj(typename DATATYPE::AdjointType* sendbufAdjoints, typename DATATYPE::PassiveType* sendbufPrimals, int sendbufSize, typename DATATYPE::AdjointType* recvbufAdjoints, typename DATATYPE::PassiveType* recvbufPrimals, int recvbufSize, int count, TAMPI_Op op, int root, TAMPI_Comm comm) {
     if(root == getRank(comm)) {
       MPI_Bcast(recvbufAdjoints, recvbufSize, MPI_BYTE, root, comm);
