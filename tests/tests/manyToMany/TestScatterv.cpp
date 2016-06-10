90a2a7fba87094cd7560bd0f6bafa0c06c2154ae
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2016-06-10 17:30:53 +0200

    Added test for scatterv

---
+++ tests/tests/manyToMany/TestScatterv.cpp
@@ -0,0 +1,17 @@
+#include <toolDefines.h>
+
+IN(20)
+OUT(10)
+POINTS(1) = {{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0}, {}}};
+SEEDS(1) = {{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0}, {11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0}}};
+
+void func(NUMBER* x, NUMBER* y) {
+  int world_rank;
+  TAMPI_Comm_rank(TAMPI_COMM_WORLD, &world_rank);
+  int world_size;
+  TAMPI_Comm_size(TAMPI_COMM_WORLD, &world_size);
+
+  int counts[2] = {10, 10};
+  int displs[2] = {0, 10};
+  medi::TAMPI_Scatterv<MPI_NUMBER, MPI_NUMBER>(x, counts, displs, y, 10, 0, MPI_COMM_WORLD);
+}
