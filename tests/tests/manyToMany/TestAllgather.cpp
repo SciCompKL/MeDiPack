b21e4e010ab4bda016d6c911517118230e7c23d3
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2016-06-14 17:09:06 +0200

    Added tests for Allgather.

---
+++ tests/tests/manyToMany/TestAllgather.cpp
@@ -0,0 +1,15 @@
+#include <toolDefines.h>
+
+IN(10)
+OUT(20)
+POINTS(1) = {{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0}, {11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0}}};
+SEEDS(1) = {{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0}, {101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, 108.0, 109.0, 110.0, 111.0, 112.0, 113.0, 114.0, 115.0, 116.0, 117.0, 118.0, 119.0, 120.0}}};
+
+void func(NUMBER* x, NUMBER* y) {
+  int world_rank;
+  TAMPI_Comm_rank(TAMPI_COMM_WORLD, &world_rank);
+  int world_size;
+  TAMPI_Comm_size(TAMPI_COMM_WORLD, &world_size);
+
+  medi::TAMPI_Allgather<MPI_NUMBER, MPI_NUMBER>(x, 10, y, 10, MPI_COMM_WORLD);
+}
