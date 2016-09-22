b3440e7553235fc87d1c4eb1dff7d32e015c42a9
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2016-09-22 14:13:06 +0200

    Added generation for int pairs.

--- include/medi/typeDefinitions.h
+++ include/medi/typeDefinitions.h
@@ -14,4 +14,19 @@ namespace medi {
     virtual ~HandleBase() {}
   };
 
+  // structures for the passive types
+
+  template <typename T>
+  struct PairWithInt {
+      T a;
+      int b;
+  };
+
+  typedef PairWithInt<float> FloatIntPair;
+  typedef PairWithInt<double> DoubleIntPair;
+  typedef PairWithInt<long> LongIntPair;
+  typedef PairWithInt<int> IntIntPair;
+  typedef PairWithInt<short> ShortIntPair;
+  typedef PairWithInt<long double> LongDoubleIntPair;
+
 }
