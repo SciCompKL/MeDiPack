2bcdff296343ea9cad36fcc025d0fd226c5fae21
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2017-02-22 15:52:32 +0100

    Added new test.

---
+++ tests/tests/misc/TestSameIndexInput.cpp
@@ -0,0 +1,26 @@
+#include <toolDefines.h>
+
+IN(1)
+OUT(10)
+POINTS(1) = {{{2.0}, {12.0}}};
+SEEDS(1) = {{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0}, {11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0}}};
+
+void func(NUMBER* x, NUMBER* y) {
+  int world_rank;
+  medi::AMPI_Comm_rank(AMPI_COMM_WORLD, &world_rank);
+  int world_size;
+  medi::AMPI_Comm_size(AMPI_COMM_WORLD, &world_size);
+
+  if(world_rank == 0) {
+    NUMBER* z = new NUMBER[10];
+    for(int i = 0; i < 10; ++i) {
+      z[i] = x[0];
+    }
+
+    medi::AMPI_Send(z, 10, mpiNumberType, 1, 42, AMPI_COMM_WORLD);
+
+    delete [] z;
+  } else {
+    medi::AMPI_Recv(y, 10, mpiNumberType, 0, 42, AMPI_COMM_WORLD, AMPI_STATUS_IGNORE);
+  }
+}
