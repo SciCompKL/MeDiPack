be45d3fe9ef3a140f32a716517935e4098e43188
Johannes Blühdorn <johannes.bluehdorn@scicomp.uni-kl.de>
2020-06-15 11:31:46 +0200

    Add tests for redundant communication completion.

---
+++ tests/tests/wait_test/TestTestWait.cpp
@@ -0,0 +1,58 @@
+/*
+ * MeDiPack, a Message Differentiation Package
+ *
+ * Copyright (C) 2017-2020 Chair for Scientific Computing (SciComp), TU Kaiserslautern
+ * Homepage: http://www.scicomp.uni-kl.de
+ * Contact:  Prof. Nicolas R. Gauger (codi@scicomp.uni-kl.de)
+ *
+ * Lead developers: Max Sagebaum (SciComp, TU Kaiserslautern)
+ *
+ * This file is part of MeDiPack (http://www.scicomp.uni-kl.de/software/codi).
+ *
+ * MeDiPack is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeDiPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU
+ * General Public License along with MeDiPack.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Max Sagebaum, Tim Albring (SciComp, TU Kaiserslautern)
+ */
+
+#include <toolDefines.h>
+
+IN(10)
+OUT(10)
+POINTS(1) = {{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0}, {11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0}}};
+SEEDS(1) = {{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0}, {11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0}}};
+
+void func(NUMBER* x, NUMBER* y) {
+  int world_rank;
+  medi::AMPI_Comm_rank(AMPI_COMM_WORLD, &world_rank);
+  int world_size;
+  medi::AMPI_Comm_size(AMPI_COMM_WORLD, &world_size);
+
+  medi::AMPI_Request request[10];
+  for(int i = 0; i < 10; ++i) {
+    if(world_rank == 0) {
+      medi::AMPI_Isend(&x[i], 1, mpiNumberType, 1, 42 + i, AMPI_COMM_WORLD, &request[i]);
+    } else {
+      medi::AMPI_Irecv(&y[i], 1, mpiNumberType, 0, 42 + i, AMPI_COMM_WORLD, &request[i]);
+    }
+  }
+
+  for(int i = 0; i < 10; ++i) {
+    int flag = false;
+    while(false == flag) {
+      medi::AMPI_Test(&request[i], &flag, AMPI_STATUS_IGNORE);
+    }
+    medi::AMPI_Wait(&request[i], AMPI_STATUS_IGNORE);
+  }
+}
